a36d74262dc53ac5ffa0330b5b03401e7dad4720
Avrumi Friedman <info@lakewoodprogramming.com>
2019-05-15 20:24:16 -0400

    Initial commit

---
+++ SignalRDemo/ChatHub.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRDemo
+{
+    public class ChatMessage
+    {
+        public string Name { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class ChatHub : Hub
+    {
+        private static int _count;
+
+        public void SendMessage(ChatMessage message)
+        {
+            //Context.User.Identity.Name
+            Clients.All.SendAsync("NewMessage", message);
+        }
+
+        public void NewUser()
+        {
+            _count++;
+            Clients.All.SendAsync("NewCount", new {count = _count});
+        }
+    }
+}
---
+++ SignalRDemo/wwwroot/js/index.js
@@ -0,0 +1,22 @@
+﻿$(() => {
+    const connection = new signalR.HubConnectionBuilder()
+        .withUrl("/chatHub").build();
+
+    connection.start().then(() => {
+        connection.invoke("NewUser");
+    });
+
+    $("#send").on('click', () => {
+        const name = $("#name").val();
+        const message = $("#message").val();
+        connection.invoke("SendMessage", { name, message });
+    });
+
+    connection.on("NewMessage", chatMessage => {
+        $("#messages").append(`<h4>${chatMessage.name} says: ${chatMessage.message}</h4>`)
+    });
+
+    connection.on("NewCount", count => {
+        $("#count").text(count.count);
+    });
+});
